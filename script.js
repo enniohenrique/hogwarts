e8fc664c30d9f8e3bfd7a2402bc54231869b2e58
enniohenrique <ennioh@icloud.com>
2022-08-30 22:45:07 -0400

    Setas Adicionadas

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const casaGrifinoria = document.querySelector('#casaGrifinoria');
+const arrowBack_grifinoria = document.querySelector('#arrowBack_grifinoria')
+const arrowBack_sonserina = document.querySelector('#arrowBack_sonserina')
+const arrowBack_corvinal = document.querySelector('#arrowBack_corvinal')
+const arrowBack_lufalufa = document.querySelector('#arrowBack_lufalufa')
+
 const casaSonserina = document.querySelector('#casaSonserina');
 const casaCorvinal = document.querySelector('#casaCorvinal');
 const casaLufaLufa = document.querySelector('#casaLufaLufa');
@@ -12,8 +17,11 @@ const active2 = () => casaSonserina.classList.toggle('active')
 const active3 = () => casaCorvinal.classList.toggle('active')
 const active4 = () => casaLufaLufa.classList.toggle('active')
 
-
 btnGrifinoria.addEventListener('click',active)
 btnSonserina.addEventListener('click',active2)
 btnCorvinal.addEventListener('click',active3)
 btnLufaLufa.addEventListener('click',active4)
+arrowBack_grifinoria.addEventListener('click',active)
+arrowBack_sonserina.addEventListener('click',active2)
+arrowBack_corvinal.addEventListener('click',active3)
+arrowBack_lufalufa.addEventListener('click',active4)
